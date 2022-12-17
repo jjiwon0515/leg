e34b504c7ce32bc313e086b7d0faf05e66bae9cd
jiwon <107084512+huboca@users.noreply.github.com>
2022-12-17 16:17:04 +0900

    Update leg_key1.asm

--- arm/leg_key1.asm
+++ arm/leg_key1.asm
@@ -1,1 +1,7 @@
-
+0x00008cd4<+0>:     push  {r11}
+0x00007cd8<+4>:     add   r11,sp,#0
+0x00007cdc<+8>:     mov   r3,pc
+0x00007ce0<+12>:    mov   r0,r3
+0x00007ce4<+16>:    sub   sp,r11,#0
+0x00007ce8<+20>:    pop   {r11}
+0x00007cec<+24>:    bx    lr
